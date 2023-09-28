0af18781a8f0536b104351a75478c4ccecce1c1e
tedycoeg <tedycoeg5000@gmail.com>
2023-09-27 21:05:20 -0700

    Memberikan default bold font ke seluruh tag

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // Memberikan Warna Pada Background di HTML
 // Memberikan Warna Pada Background di Javascript
 document.body.style.backgroundColor = "blue";
+document.body.style.fontWeight = "bold";
